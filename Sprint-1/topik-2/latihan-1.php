478ab93199e2a6f29624d61e95072be517bc7ccb
rafi300604 <yahya.acpridan@gmail.com>
2022-07-15 13:57:07 +0700

    first commit

---
+++ Sprint-1/topik-2/latihan-1.php
@@ -0,0 +1,7 @@
+<?php
+
+$panjang = 10;
+$lebar = 20;
+
+$luasPersegiPanjang = $panjang * $lebar;
+echo $hasilPerhitungan = $luasPersegiPanjang;
---
+++ Sprint-1/topik-2/latihan-3.php
@@ -0,0 +1,5 @@
+<?php
+
+$celsius = 34;
+
+echo $celsiusMenjadiReamur = 4 / 5 * $celsius;
