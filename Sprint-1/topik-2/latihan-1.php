77d4f143dd2984cc9adc94045006142143cc41a5
rafi300604 <yahya.acpridan@gmail.com>
2022-07-15 20:01:58 +0700

    modified topik 2

--- Sprint-1/topik-2/latihan-1.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-$panjang = 10;
-$lebar = 20;
-
-$luasPersegiPanjang = $panjang * $lebar;
-echo $hasilPerhitungan = $luasPersegiPanjang;
--- Sprint-1/topik-2/latihan-3.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-$celsius = 34;
-
-echo $celsiusMenjadiReamur = 4 / 5 * $celsius;
