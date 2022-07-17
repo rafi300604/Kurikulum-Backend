1f0e6a66a0f5a65f80d9eb341f4c45040dade550
rafi300604 <yahya.acpridan@gmail.com>
2022-07-17 09:48:34 +0700

    modified latihan-2

--- Sprint-1/Topik-3/latihan-2.php
+++ Sprint-1/Topik-3/latihan-2.php
@@ -1,11 +1,8 @@
 <?php
 
-//n = 9, ketika nanti saat ditampilkan sudah ada perhitungannya jadi tidak usah dihitung lagi, maka dari itu jumlah deret aritmatika ada 9 bilangan (n = 9)
-$n = 9;
 
-
-for ($i = 0; $i <= $n; $i++) {
-    if ($i == $n) {
+for ($i = 0; $i <= 4; $i++) {
+    if ($i == 4) {
         continue;
     }
     echo "" . ($i * 2 + 1) . " ";
